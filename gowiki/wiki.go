9785536656f5e72479e091ab34a82be9c9696340
ArturKlajnerok <a.klajnerok@gmail.com>
2014-10-16 19:46:42 +0200

    Added webserver support to show pages

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,32 +1,37 @@
 package main
 
 import (
-        "fmt"
-        "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type Page struct {
-    Title string
-    Body  []byte
+	Title string
+	Body  []byte
 }
 
 func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return ioutil.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-    page1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    page1.save()
-    page2, _ := loadPage("TestPage")
-    fmt.Println(string(page2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
