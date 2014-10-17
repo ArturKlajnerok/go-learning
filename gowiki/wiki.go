a85914e3231373da7b2fd32bfc73890093c6ee05
ArturKlajnerok <a.klajnerok@gmail.com>
2014-10-17 13:46:02 +0200

    Editing pages in html form

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -36,7 +36,22 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	http.ListenAndServe(":8080", nil)
 }
