7f37c1fd8200a29b027ecc91b16010c8671062d5
ArturKlajnerok <a.klajnerok@gmail.com>
2014-10-17 19:03:58 +0200

    Saving pages

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -44,6 +44,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	t, _ := template.ParseFiles(tmpl + ".html")
 	t.Execute(w, p)
@@ -52,5 +60,6 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
