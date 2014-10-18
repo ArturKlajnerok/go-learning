da0552e7715199f1326e57260573bcb8bdfa3c2a
ArturKlajnerok <a.klajnerok@gmail.com>
2014-10-18 19:12:54 +0200

    Error handling for saving page

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -48,7 +48,11 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
