d867cd0f99698b1c946b6698b038e75ac0635323
ArturKlajnerok <a.klajnerok@gmail.com>
2014-10-30 12:00:16 +0100

    Encoding json to interface

--- json-and-go/main.go
+++ json-and-go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	//"fmt"
 	"log"
 	//"os"
 )
@@ -14,19 +15,30 @@ type Message struct {
 
 var m Message
 var mu Message
+var mi Message
 
 func main() {
 	m := Message{"Alice", "Hello", 1294706395881547000}
 
+	// Encode
 	b, err_ma := json.Marshal(m)
 	if err_ma == nil {
 		log.Printf("%s", b)
 		//os.Stdout.Write(b)
 	}
 
+	// Decode to struct
 	err_um := json.Unmarshal(b, &mu)
 
 	if err_um == nil {
 		log.Printf("%s", mu.Name)
 	}
+
+	// Decoding to interface
+	var inter interface{}
+	err_in := json.Unmarshal(b, &inter)
+	if err_in == nil {
+		log.Printf("%s", inter)
+	}
+
 }
