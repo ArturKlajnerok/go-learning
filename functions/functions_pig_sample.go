7f828089c415a4694d5091af1969b2a7ba0eb5a4
ArturKlajnerok <a.klajnerok@gmail.com>
2014-11-08 23:07:21 +0100

    Rules and structs for functions pig sample

---
+++ functions/functions_pig_sample.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	win            = 100 // The winning score in a game of Pig
+	gamesPerSeries = 10  // The number of games per series to simulate
+)
+
+// A score includes scores accumulated in previous turns for each player,
+// as well as the points scored by the current player in this turn.
+type score struct {
+	player, opponent, thisTurn int
+}
+
+func main() {
+	fmt.Printf("Hello pig")
+}
