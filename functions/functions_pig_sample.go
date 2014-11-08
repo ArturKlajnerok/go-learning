2c5d32168541fb42a8fbb0b8dabd41f83fe680b1
ArturKlajnerok <a.klajnerok@gmail.com>
2014-11-08 23:51:06 +0100

    Roll and Stay funtions

--- functions/functions_pig_sample.go
+++ functions/functions_pig_sample.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 const (
@@ -18,6 +19,29 @@ type score struct {
 // An action transitions stochastically to a resulting score.
 type action func(current score) (result score, turnIsOver bool)
 
+// roll returns the (result, turnIsOver) outcome of simulating a die roll.
+// If the roll value is 1, then thisTurn score is abandoned, and the players'
+// roles swap.  Otherwise, the roll value is added to thisTurn.
+func roll(s score) (score, bool) {
+	outcome := rand.Intn(6) + 1 // A random int in [1, 6]
+	if outcome == 1 {
+		return score{s.opponent, s.player, 0}, true
+	}
+	return score{s.player, s.opponent, outcome + s.thisTurn}, false
+}
+
+// stay returns the (result, turnIsOver) outcome of staying.
+// thisTurn score is added to the player's score, and the players' roles swap.
+func stay(s score) (score, bool) {
+	return score{s.opponent, s.player + s.thisTurn, 0}, true
+}
+
 func main() {
+	var s score
+	s, end := roll(s)
 	fmt.Printf("Hello pig")
+	if !end {
+		fmt.Printf("%i", s.thisTurn)
+	}
+
 }
