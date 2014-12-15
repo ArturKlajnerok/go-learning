6a930af7f956a2230c1d0e1f7d5b720d8f2fb91b
ArturKlajnerok <a.klajnerok@gmail.com>
2014-12-15 20:14:30 +0100

    Controller for db operations

---
+++ revel/auction/app/controllers/gorp.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"database/sql"
+	"github.com/coopernurse/gorp"
+	"github.com/revel/revel"
+)
+
+var (
+	Dbm *gorp.DbMap
+)
+
+type GorpController struct {
+	*revel.Controller
+	Txn *gorp.Transaction
+}
+
+func (c *GorpController) Begin() revel.Result {
+	txn, err := Dbm.Begin()
+	if err != nil {
+		panic(err)
+	}
+	c.Txn = txn
+	return nil
+}
+
+func (c *GorpController) Commit() revel.Result {
+	if c.Txn == nil {
+		return nil
+	}
+	if err := c.Txn.Commit(); err != nil && err != sql.ErrTxDone {
+		panic(err)
+	}
+	c.Txn = nil
+	return nil
+}
+
+func (c *GorpController) Rollback() revel.Result {
+	if c.Txn == nil {
+		return nil
+	}
+	if err := c.Txn.Rollback(); err != nil && err != sql.ErrTxDone {
+		panic(err)
+	}
+	c.Txn = nil
+	return nil
+}
