76d78bd4b9d25caa64c7513cfc9aba05b1329c1a
ArturKlajnerok <a.klajnerok@gmail.com>
2014-12-15 19:53:50 +0100

    Validating auction bid item model

--- revel/auction/app/models/bid-item.go
+++ revel/auction/app/models/bid-item.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"github.com/revel/revel"
+	"regexp"
+)
+
 type BidItem struct {
 	Id             int64   `db:"id" json:"id"`
 	Name           string  `db:"name" json:"name"`
@@ -8,4 +13,26 @@ type BidItem struct {
 	StartBid       float32 `db:"start_bid" json:"start_bid"`
 	BidIncrement   float32 `db:"bid_incr" json:"bid_incr"`
 	InstantBuy     float32 `db:"inst_buy" json:"inst_buy"`
+}
+
+func (b *BidItem) Validate(v *revel.Validation) {
+
+	v.Check(b.Name,
+		revel.ValidRequired(),
+		revel.ValidMaxSize(25))
+
+	v.Check(b.Category,
+		revel.ValidRequired(),
+		revel.ValidMatch(
+			regexp.MustCompile(
+				"^(travel|leasure|sports|entertainment)$")))
+
+	v.Check(b.EstimatedValue,
+		revel.ValidRequired())
+
+	v.Check(b.StartBid,
+		revel.ValidRequired())
+
+	v.Check(b.BidIncrement,
+		revel.ValidRequired())
 }
