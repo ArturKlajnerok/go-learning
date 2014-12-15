0d51e9563b1a6a51236c1f8465dbadceded24997
ArturKlajnerok <a.klajnerok@gmail.com>
2014-12-15 20:35:40 +0100

    Config support for db

--- revel/auction/app/controllers/init.go
+++ revel/auction/app/controllers/init.go
@@ -7,3 +7,33 @@ func init() {
 	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
 	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
 }
+
+func getParamString(param string, defaultValue string) string {
+	p, found := revel.Config.String(param)
+	if !found {
+		if defaultValue == "" {
+			revel.ERROR.Fatal("Cound not find parameter: " + param)
+		} else {
+			return defaultValue
+		}
+	}
+	return p
+}
+
+func getConnectionString() string {
+	host := getParamString("db.host", "")
+	port := getParamString("db.port", "3306")
+	user := getParamString("db.user", "")
+	pass := getParamString("db.password", "")
+	dbname := getParamString("db.name", "auction")
+	protocol := getParamString("db.protocol", "tcp")
+	dbargs := getParamString("dbargs", " ")
+
+	if strings.Trim(dbargs, " ") != "" {
+		dbargs = "?" + dbargs
+	} else {
+		dbargs = ""
+	}
+	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s",
+		user, pass, protocol, host, port, dbname, dbargs)
+}
