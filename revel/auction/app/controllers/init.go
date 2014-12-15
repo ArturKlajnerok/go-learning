a5f0ede933f1cac700aa33ac56d344446ed5ea5d
ArturKlajnerok <a.klajnerok@gmail.com>
2014-12-15 20:17:18 +0100

    Init gorp controller

---
+++ revel/auction/app/controllers/init.go
@@ -0,0 +1,9 @@
+package controllers
+
+import "github.com/revel/revel"
+
+func init() {
+	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
+	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
+	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
+}
