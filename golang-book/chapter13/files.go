1d33aa487a742b6fb4b99b26fdb42abf0ddf36f9
ArturKlajnerok <a.klajnerok@gmail.com>
2015-01-05 22:15:23 +0100

    Goolang book chaper: 13 (reading directory)

--- golang-book/chapter13/files.go
+++ golang-book/chapter13/files.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -46,4 +47,25 @@ func main() {
 	defer file2.Close()
 
 	file2.WriteString("Go new test")
+
+	// read directory
+	dir, err := os.Open(".")
+	if err != nil {
+		return
+	}
+	defer dir.Close()
+
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		return
+	}
+	for _, fi := range fileInfos {
+		fmt.Println(fi.Name())
+	}
+
+	// read directory recursively
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
+		return nil
+	})
 }
